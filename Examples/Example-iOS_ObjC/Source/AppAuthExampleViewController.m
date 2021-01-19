9cc698f1e3a21880a5cc4db4e4fd61b805772efc
Ming Li <ftntming@gmail.com>
2021-01-18 18:15:38 -0800

    make objective c example work

--- Examples/Example-iOS_ObjC/Source/AppAuthExampleViewController.m
+++ Examples/Example-iOS_ObjC/Source/AppAuthExampleViewController.m
@@ -28,20 +28,20 @@ typedef void (^PostRegistrationCallback)(OIDServiceConfiguration *configuration,
 
 /*! @brief The OIDC issuer from which the configuration will be discovered.
  */
-static NSString *const kIssuer = @"https://issuer.example.com";
+static NSString *const kIssuer = @"https://sso.fortivoice-cloud.net/auth/realms/chat.vanaaa.ca";
 
 /*! @brief The OAuth client ID.
     @discussion For client configuration instructions, see the README.
         Set to nil to use dynamic registration with this example.
     @see https://github.com/openid/AppAuth-iOS/blob/master/Examples/Example-iOS_ObjC/README.md
  */
-static NSString *const kClientID = @"YOUR_CLIENT_ID";
+static NSString *const kClientID = @"barkoder-frontend";
 
 /*! @brief The OAuth redirect URI for the client @c kClientID.
     @discussion For client configuration instructions, see the README.
     @see https://github.com/openid/AppAuth-iOS/blob/master/Examples/Example-iOS_ObjC/README.md
  */
-static NSString *const kRedirectURI = @"com.example.app:/oauth2redirect/example-provider";
+static NSString *const kRedirectURI = @"maslick://oauthresponse";
 
 /*! @brief NSCoding key for the authState property.
  */
